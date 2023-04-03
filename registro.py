4fdc5bda8317f05fd48c249402b2b51c491e273d
Augusto Barrios <augustobg02@gmail.com>
2023-04-03 19:48:32 -0300

    Second Commit

--- registro.py
+++ registro.py
@@ -1,7 +1,7 @@
 import json
 ruta = "C:/Users/augus/OneDrive/Escritorio/Primera pre-entrega + Barrios/"
 
-def registro():
+""" def registro():
     usuario =  input("Ingresa tu usuario")
     contraseña = input("Ingresa una contraseña")
     
@@ -16,4 +16,22 @@ def registro():
     with open(ruta+ 'baseDeDAtos.json', 'w') as file:
         json.dump(datos_a_leer, file, indent=2)
 
-registro()
+registro() """ 
+
+def login():
+    usuario =  input("Ingresa tu usuario")
+    
+    
+    with open(ruta + 'baseDeDAtos.json', 'r', encoding='utf-8') as file:
+        datos_a_leer = json.load(file)
+        for lista in datos_a_leer:
+            if usuario == lista["Usuario"]:
+                print( f"Ahora brindanos tu contraseña querido/a {usuario}")
+                contraseña = input("Ingresa una contraseña")
+                if contraseña == lista["contrasena"]:
+                    print(f"Bienvenido {usuario}")
+                else: print("O te equivocaste de contraseña o eres un hacker, cual sera la opcion correcta?")
+            else:()
+
+
+login()
